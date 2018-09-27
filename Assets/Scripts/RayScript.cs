915713bec783854661dbb8ff0e6a27bb6b8814d1
shota-fukuoka <ponta.shota.fukuoka@gmail.com>
2018-09-27 10:51:42 +0900

    デバッグ用にマウス操作実装

---
+++ Assets/Scripts/RayScript.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RayScript : MonoBehaviour
+{
+
+    private static readonly Joycon.Button[] m_buttons =
+    Enum.GetValues(typeof(Joycon.Button)) as Joycon.Button[];
+
+    private List<Joycon> m_joycons;
+    private Joycon m_joyconL;
+    private Joycon m_joyconR;
+    private Joycon.Button? m_pressedButtonL;
+    private Joycon.Button? m_pressedButtonR;
+
+    // 照準のImageをインスペクターから設定
+    [SerializeField]
+    private Image aimPointImage;
+
+    private void Start()
+    {
+        m_joycons = JoyconManager.Instance.j;
+
+        if (m_joycons == null || m_joycons.Count <= 0) return;
+
+        m_joyconL = m_joycons.Find(c => c.isLeft);
+        m_joyconR = m_joycons.Find(c => !c.isLeft);
+    }
+
+    void Update()
+    {
+        var orientation = m_joyconL.GetVector().eulerAngles;
+        var angles = transform.localEulerAngles;
+        angles.x = -orientation.x;
+        angles.y = orientation.z;
+        angles.z = orientation.y;
+        //angles.z = orientation.z;
+        transform.localEulerAngles = angles;
+
+        if (m_joyconL.GetButton(Joycon.Button.DPAD_RIGHT))
+        {
+            transform.localRotation = new Quaternion(0, 0, 0, 0);
+        }
+
+        // Rayを飛ばす（第1引数がRayの発射座標、第2引数がRayの向き）
+        Ray ray = new Ray(transform.position, transform.forward);
+
+        // outパラメータ用に、Rayのヒット情報を取得するための変数を用意
+        RaycastHit hit;
+
+        // シーンビューにRayを可視化するデバッグ（必要がなければ消してOK）
+        Debug.DrawRay(ray.origin, ray.direction * 30.0f, Color.red, 0.0f);
+
+        // Rayのhit情報を取得する
+        if (Physics.Raycast(ray, out hit, 30.0f))
+        {
+
+            // Rayがhitしたオブジェクトのタグ名を取得
+            string hitTag = hit.collider.tag;
+
+            // タグの名前がEnemyだったら、照準の色が変わる
+            if ((hitTag.Equals("Enemy")))
+            {
+                //照準を赤に変える
+                aimPointImage.color = new Color(1.0f, 0.0f, 0.0f, 1.0f);
+            }
+            else
+            {
+                // Enemy以外では水色に
+                aimPointImage.color = new Color(0.0f, 1.0f, 1.0f, 1.0f);
+            }
+
+        }
+        else
+        {
+            // Rayがヒットしていない場合は水色に
+            aimPointImage.color = new Color(0.0f, 1.0f, 1.0f, 1.0f);
+        }
+    }
+}
---
+++ Assets/Scripts/ShotCursor.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShotCursor : MonoBehaviour
+{
+
+    //　カーソルに使用するテクスチャ
+    [SerializeField]
+    private Texture2D cursor;
+
+    [SerializeField]
+    GameObject BulletObject;
+
+    [SerializeField]
+    float VecPow = 0.0f;
+
+    void Start()
+    {
+        //　カーソルを自前のカーソルに変更
+        Cursor.SetCursor(cursor, new Vector2(cursor.width / 2, cursor.height / 2), CursorMode.ForceSoftware);
+    }
+
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        transform.rotation = Quaternion.LookRotation(ray.direction);
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 1f, LayerMask.GetMask("Gun")))
+        {
+            Cursor.visible = false;
+        }
+        else
+        {
+            Cursor.visible = true;
+        }
+
+        //　マウスの左クリックで撃つ
+        if (Input.GetButtonDown("Fire1"))
+        {
+            GameObject newBullet =  Instantiate(BulletObject, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+            //Shot();
+            Vector3 force = transform.forward * VecPow;
+
+            newBullet.GetComponent<Rigidbody>().AddForce(force);
+        }
+    }
+
+    //　敵を撃つ
+    void Shot()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 100f, LayerMask.GetMask("Enemy")))
+        {
+            Destroy(hit.collider.gameObject);
+        }
+    }
+}
