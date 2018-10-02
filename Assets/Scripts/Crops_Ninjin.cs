a692fe68bbd6532379d000902c380800cb5bd764
kayuusan <kayuusan@gmail.com>
2018-10-02 18:58:16 +0900

    ・畑ゲージのパーセント表示 ・Cropsタグの追加（農作物）

---
+++ Assets/Scripts/Crops_Ninjin.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crops_Ninjin : MonoBehaviour {
+
+    public int m_Life = 5;
+    float m_Rot_Y = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        m_Rot_Y += 1.0f;
+        transform.rotation = Quaternion.Euler(45.0f, m_Rot_Y, 45.0f);
+        if( m_Life <= 0)
+        {
+            Destroy(this);
+        }
+	}
+
+    // ダメージ処理
+    public void Damage(int Damage)
+    {
+        m_Life -= Damage;
+    }
+}
--- Assets/Scripts/UI/UI_Farm.cs
+++ Assets/Scripts/UI/UI_Farm.cs
@@ -5,36 +5,44 @@ using UnityEngine.UI;
 
 public class UI_Farm : MonoBehaviour {
 
-    public Slider m_Farm_UI;
-    public int m_Farm_HP = 100;
-    public Text m_Farm_Text;
+    // デバッグ用変数
+    public Text Debug_HP_Text;      // 残りの農作物
+    public Text Debug_HP_MAX_Text;  // 農作物の総数
+
+    public Slider m_Farm_UI;        // 畑ゲージ
+    public Text m_PercentHP_Text;   // パーセントのテキスト
+
+    float m_HP = 100;        // 畑HP
+    float m_MAX_HP = 100;     // MAX HP
 
     void Start()
     {
-        m_Farm_Text.text = m_Farm_HP.ToString();
+        // 農作物をカウントし、代入
+        m_HP = GameObject.FindGameObjectsWithTag("Crops").Length;
+        m_MAX_HP = m_HP;
+        // 残数表示
+        Debug_HP_Text.text = Mathf.FloorToInt(m_HP).ToString();
+        // 残数代入
+        m_Farm_UI.value = m_HP;
+
+        // 農作物総数の表示
+        Debug_HP_MAX_Text.text = Mathf.FloorToInt(m_MAX_HP).ToString() + "/";
+        // 畑ゲージのMAXゲージの代入
+        m_Farm_UI.maxValue = m_MAX_HP;
+
+        // パーセントの初期化
+        m_PercentHP_Text.text = "100%";
     }
 
     void Update()
     {
-    }
-    
-    public void SetValue( int Value)
-    {
-        m_Farm_HP = Value;
-        m_Farm_UI.value = m_Farm_HP;
-        m_Farm_Text.text = m_Farm_HP.ToString();
-    }
+        // 
+        m_HP = GameObject.FindGameObjectsWithTag("Crops").Length;
+        Debug_HP_Text.text = Mathf.FloorToInt(m_HP).ToString();
+        m_Farm_UI.value = m_HP;
 
-    public void Damage(int Value)
-    {
-        m_Farm_HP -= Value;
-        if (m_Farm_HP < 0)
-        {
-            m_Farm_HP = 0;
-            return;
-        }
-        m_Farm_UI.value = m_Farm_HP;
-        m_Farm_Text.text = m_Farm_HP.ToString();
+        // パーセントの算出
+        int percentHP = Mathf.FloorToInt(m_HP / m_MAX_HP * 100.0f);
+        m_PercentHP_Text.text = percentHP.ToString() + "%";
     }
-
 }
