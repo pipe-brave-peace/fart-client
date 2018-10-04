3a2eefc7afbb47fc672b04882ff09366c89be3fc
kayuusan <kayuusan@gmail.com>
2018-10-04 18:42:59 +0900

    ・フェーズ情報の作成 ・フェーズごと農作物の数を取得処理を追加 ・UIマネージャーの消去

--- Assets/Scripts/UI/UI_Farm.cs
+++ Assets/Scripts/UI/UI_Farm.cs
@@ -5,27 +5,71 @@ using UnityEngine.UI;
 
 public class UI_Farm : MonoBehaviour {
 
-    public Text HP_Text;      // 残りの農作物
-    public Text HP_MAX_Text;  // 農作物の総数
-    float m_HP = 100;         // 畑HP
-    float m_MAX_HP = 100;     // MAX HP
+    public Text m_HP_Text;      // 残りの農作物
+    public Text m_HP_MAX_Text;  // 農作物の総数
+    public Phase[] m_Phase;     // フェーズ情報の代入
+
+    float m_HP = 100;           // 畑HP
+    float m_MAX_HP = 100;       // MAX HP
+    Phase m_NowPhase;           // 現在のフェーズ情報
 
     // Use this for initialization
     void Start () {
+        // 初期フェーズ情報の代入
+        m_NowPhase = m_Phase[0];
+
         // 農作物をカウントし、代入
-        m_HP = GameObject.FindGameObjectsWithTag("Crops").Length;
-        m_MAX_HP = m_HP;
+        m_HP = m_NowPhase.GetCropsChildCount();
+        m_MAX_HP = m_NowPhase.GetMaxCropsChildCount();
 
         // 残数表示
-        HP_Text.text = Mathf.FloorToInt(m_HP).ToString();
+        m_HP_Text.text = Mathf.FloorToInt(m_HP).ToString();
         // 農作物総数の表示
-        HP_MAX_Text.text = "/"+ Mathf.FloorToInt(m_MAX_HP).ToString();
+        m_HP_MAX_Text.text = "/"+ Mathf.FloorToInt(m_MAX_HP).ToString();
     }
 
     // Update is called once per frame
     void Update () {
         // 残り数のカウント
-        m_HP = GameObject.FindGameObjectsWithTag("Crops").Length;
-        HP_Text.text = Mathf.FloorToInt(m_HP).ToString();
+        m_HP = m_NowPhase.GetCropsChildCount();
+        m_HP_Text.text = Mathf.FloorToInt(m_HP).ToString();
+
+        // デバッグ用キー押し判定
+        if( Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SetPhase(0);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            SetPhase(1);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            SetPhase(2);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            SetPhase(3);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            SetPhase(4);
+        }
+    }
+
+    // 
+    public void SetPhase( int Phase)
+    {
+        // フェーズ情報の代入
+        m_NowPhase = m_Phase[Phase];
+
+        // 農作物をカウントし、代入
+        m_HP = m_NowPhase.GetCropsChildCount();
+        m_MAX_HP = m_NowPhase.GetMaxCropsChildCount();
+
+        // 残数表示
+        m_HP_Text.text = Mathf.FloorToInt(m_HP).ToString();
+        // 農作物総数の表示
+        m_HP_MAX_Text.text = "/" + Mathf.FloorToInt(m_MAX_HP).ToString();
     }
 }
--- Assets/Scripts/Utility/Billboard.cs
+++ Assets/Scripts/Utility/Billboard.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class Billboard : MonoBehaviour {
 
+    // 対象カメラ
     public Camera targetCamera;
 
     void Update()
     {
+        // 対象カメラに向く
         transform.LookAt(transform.position + targetCamera.transform.rotation * Vector3.forward,
                          targetCamera.transform.rotation * Vector3.up);
     }
