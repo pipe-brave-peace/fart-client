ff8546648941c64fe73e322f4a449c5c749f64ab
kayuusan <kayuusan@gmail.com>
2018-10-18 20:25:33 +0900

    カメムシ攻撃時のエフェクトの作成

---
+++ Assets/Scripts/Effect/Effect_Inc.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Effect_Inc : MonoBehaviour {
+
+    [SerializeField]
+    Sprite[] m_ImageList;       // テクスチャ素材
+
+    private RectTransform m_Image;  // 位置、サイズ更新用
+    private Image m_ImageColor;     // 色、テクスチャ更新
+
+    private Vector2 m_Size;         // サイズ
+    private Vector3 m_Pos;          // 座標
+    private Color m_Color;          // 色
+    private int m_Mode;             // モード
+    private float m_Timer;          // カウンター
+
+    // Use this for initialization
+    void Start () {
+        int ImageID = Mathf.RoundToInt( Random.Range(0, m_ImageList.Length));
+        m_Image = GetComponent<RectTransform>();
+        m_ImageColor = GetComponent<Image>();
+
+        m_ImageColor.sprite = m_ImageList[ImageID];       // テクスチャの挿入
+        m_Size = m_Image.sizeDelta;                       // サイズの記憶
+        m_Pos = m_Image.transform.position;               // 座標の記憶
+        m_Image.sizeDelta = m_Size * 0.8f;                // 初期サイズの代入
+        m_Color = m_ImageColor.color;                     // 色の記憶
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        switch(m_Mode)
+        {
+            case 0:
+                // だんだん大きく
+                m_Image.sizeDelta *= 1.1f;
+                // 元のサイズになった？
+                if(m_Image.sizeDelta.x >= m_Size.x)
+                {
+                    m_Image.sizeDelta = m_Size;
+                    m_Mode++;       // 次へ
+                }
+                break;
+            case 1:
+                // サイズの更新
+                m_Size.y += 1.0f;
+                m_Size.x -= 0.1f;
+                // 座標の更新
+                m_Pos.y -= 0.5f;
+                // 数値の代入
+                m_Image.sizeDelta = m_Size*0.99f;
+                m_Image.transform.position = m_Pos;
+
+                // カウント
+                m_Timer += Time.deltaTime;
+                if(m_Timer >= 2.0f)
+                {
+                    m_Mode++;   // 次へ
+                }
+                break;
+            case 2:
+                // サイズの更新
+                m_Size.y += 1.0f;
+                m_Size.x -= 0.1f;
+                // 座標の更新
+                m_Pos.y -= 0.5f;
+                // 数値の代入
+                m_Image.sizeDelta = m_Size * 0.99f;
+                m_Image.transform.position = m_Pos;
+
+                // アルファ値の更新
+                m_Color.a = Mathf.Max(0.0f, m_Color.a - 0.005f);
+                m_ImageColor.color = m_Color;
+                // 透明になった？
+                if (m_Color.a <= 0.0f)
+                {
+                    Destroy(gameObject);    // 終了
+                    return;
+                }
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/Effect/Effect_Kamemusi.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Effect_Kamemusi : MonoBehaviour {
+
+    [SerializeField]
+    GameObject m_Canvas;
+    [SerializeField]
+    GameObject m_Inc;           // インク
+    [SerializeField]
+    Vector2 m_IncNum;           // インク数の範囲
+    [SerializeField]
+    Vector2 m_IncScl;           // インクの大きさ
+    [SerializeField]
+    GameObject m_MoveObj;       // 弾
+    [SerializeField]
+    GameObject m_TargetObj;     // 目的
+    [SerializeField]
+    private float m_MoveSpeed;  // 移動スピード
+
+    private int m_IncCnt;           // インクの数
+    private float m_Timer;          // カウンター
+    private int m_Mode;             // モード
+    private Vector3 m_MoveVector;   // 移動ベクトル
+
+    // 初期化
+    void Start ()
+    {
+        m_Timer = 0.0f;
+        m_IncCnt = Mathf.CeilToInt(Random.Range(m_IncNum.x, m_IncNum.y));   // インク数の代入
+        m_Mode = 0;
+        // 移動量の代入
+        m_MoveVector = m_TargetObj.transform.position - m_MoveObj.transform.position;
+        m_MoveVector.Normalize();
+        m_MoveVector *= m_MoveSpeed;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        switch(m_Mode)
+        {
+            case 0:
+                // 弾の移動
+                m_MoveObj.transform.position += m_MoveVector;
+                // 近い？
+                if(Vector3.Distance(m_MoveObj.transform.position, m_TargetObj.transform.position) <= 0.1f)
+                {
+                    // 弾を消す
+                    Destroy(m_MoveObj.gameObject);
+                    m_Mode++;   // 次へ
+                }
+                break;
+            case 1:
+                if (m_IncCnt <= 0) return;
+                m_Timer -= Time.deltaTime;      // カウント
+                if (m_Timer > 0.0f) return;
+                m_Timer = Random.Range(0.1f, 0.5f); // 次のインクの出るタイミング
+                m_IncCnt--;                         // インク数のカウント
+
+                // インクの生成
+                GameObject inc = (GameObject)Instantiate(m_Inc);
+                inc.transform.SetParent(m_Canvas.transform, false);
+
+                // 生成したインクの配置
+                inc.transform.position = new Vector3(Random.Range(0.0f, Screen.width), Random.Range(0.0f, Screen.height), 0.0f);
+                float size = Random.Range(m_IncScl.x, m_IncScl.y);
+                inc.transform.localScale = new Vector3(size, size, 0.0f);
+                break;
+        }
+    }
+}
