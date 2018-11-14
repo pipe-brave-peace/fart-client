0616c0bf12abdb79d732fbb966313c664d5cbfc2
kayuusan <kayuusan@gmail.com>
2018-11-14 16:40:15 +0900

    農作物の親子構造の変更

---
+++ Assets/Scripts/Crops.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Crops : MonoBehaviour {
+    
+    [SerializeField]
+    GameObject UI_HP;      // 残りの農作物
+    [SerializeField]
+    GameObject m_Soil;
+
+    private Life m_Life;
+
+    // Use this for initialization
+    void Start () {
+        m_Life = GetComponent<Life>();
+        UI_HP.SetActive(false);
+        UI_HP.GetComponent<TextMesh>().text = "　"+m_Life.GetLife().ToString();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        UI_HP.GetComponent<TextMesh>().text = "　" + m_Life.GetLife().ToString();
+        if(m_Life.isDamage())
+        {
+            UI_HP.SetActive(true);
+        }
+    }
+    private void OnDestroy()
+    {
+        m_Soil.transform.parent = transform.parent;
+    }
+}
